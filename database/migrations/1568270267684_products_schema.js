91bc010cc1a33f19fed7b79d4971e3dc76114f70
EddRaRi <163209@ids.upchiapas.edu.mx>
2019-10-05 23:59:19 -0500

    arreglo de url

--- database/migrations/1568270267684_products_schema.js
+++ database/migrations/1568270267684_products_schema.js
@@ -11,7 +11,7 @@ class ProductsSchema extends Schema {
       table.integer('code').notNullable()
       table.string('name',150).notNullable()
       table.text('description').notNullable()
-      table.string('image',150).notNullable()
+      table.text('image').notNullable()
 
       table.timestamps()
     })
